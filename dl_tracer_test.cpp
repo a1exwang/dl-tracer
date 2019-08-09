630ce9245e48b0f08b5edb035949b86475625431
Alex Wang <ice_b0und@hotmail.com>
2019-08-09 16:17:58 +0800

    I do not know why setting random value with _U_dyn_register works

--- dl_tracer_test.cpp
+++ dl_tracer_test.cpp
@@ -4,7 +4,10 @@
 #include <sys/fcntl.h>
 #include <string.h>
 
+#include <libunwind.h>
+
 extern "C" ssize_t read1(int __fd, void *__buf, size_t __nbytes);
+extern "C" void read1_end();
 
 int main() {
   int a;
@@ -16,6 +19,27 @@ int main() {
     perror("open");
     abort();
   }
+
+  unw_dyn_info_t info;
+  info.start_ip = (unw_word_t)read1;
+  info.end_ip = (unw_word_t)read1_end;
+  info.format = UNW_INFO_FORMAT_DYNAMIC;
+  info.gp = 0;
+  auto *pi = &info.u.pi;
+  pi->name_ptr = (unw_word_t)"read1";
+  pi->handler = 0;
+  pi->flags = 0;
+
+  size_t op_count = 32;
+  unw_dyn_region_info_t *region = new unw_dyn_region_info_t + sizeof(unw_dyn_op_t) * (op_count - 1);
+  region->next = nullptr;
+  region->insn_count = 10;
+  region->op_count = op_count;
+//  region->op[0]
+
+  pi->regions = region;
+
+  _U_dyn_register(&info);
 
   auto nread = read1(fd, buf, sizeof(buf));
   if (nread < 0) {
--- stack_dump.cpp
+++ stack_dump.cpp
@@ -1,10 +1,18 @@
+#define BACKWARD_HAS_UNWIND 0
+#define BACKWARD_HAS_UNWIND 0
+#define BACKWARD_HAS_BACKTRACE 1
+#define BACKWARD_HAS_BFD 1
 #include <backward.hpp>
 
 extern "C" {
   void stack_dump() {
     printf("stack dump\n");
     using namespace backward;
-    StackTrace st; st.load_here(32);
-    Printer p; p.print(st);
+    StackTrace st;
+    st.skip_n_firsts(3);
+    st.load_here(32);
+    Printer p;
+    p.snippet = false;
+    p.print(st);
   }
 }
